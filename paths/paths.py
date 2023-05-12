f9fe22306c7d3be4a4954aff38f5705d4d2ed6fd
mmosc <ma.moscati@gmail.com>
2023-05-12 09:34:36 +0200

    Add paths

---
+++ paths/paths.py
@@ -0,0 +1,8 @@
+import sys
+BASE_FOLDER = ''
+sys.path.append('..')
+datadir = f"{BASE_FOLDER}/necessary_files/"
+data_path = f'{BASE_FOLDER}/actr_data/'
+
+
+FULL_EVENTS_PATH = f'{data_path}/listening_events.tsv.bz2'
